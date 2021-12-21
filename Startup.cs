a07dbf9c19691d6d8cb5a47b9c42186c21c22357
AshaThor <smartyparty101@hotmail.com>
2021-12-21 00:02:04 +0000

    AWS + Postgres
    to work please use DATABASE_URL env var

--- Startup.cs
+++ Startup.cs
@@ -27,12 +27,46 @@ namespace PostgresConnect
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddDbContext<AshsDbContext>(options => 
+            //Database Tomfoolery
+            string connectionString = null;
+            //Get the Database environment var
+            string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
+            //Check the env ver was set if nto then use local database
+            if (string.IsNullOrEmpty(envVar))
             {
-                options.UseInMemoryDatabase("AshsDatabase");
-            });
+                connectionString = Configuration.GetConnectionString("AshsDbContext");
+            }
+            else
+            {
+                //Here is where the fun begins
+                var uri = new Uri(envVar);
+                //Extract user into 
+                var username = uri.UserInfo.Split(':')[0];
+                var password = uri.UserInfo.Split(':')[1];
+                //Extract host
+                var host = uri.Host;
+                //Build string
+                connectionString =
+                    //This line is important and was missing from the tutorial @
+                    // https://daniellethurow.com/blog/2020/7/16/connecting-to-heroku-postgresql-with-net-core
+                    "Host=" + host +
+                    "; Database=" + uri.AbsolutePath.Substring(1) +
+                    "; Username=" + username +
+                    "; Password=" + password +
+                    "; Port=" + uri.Port +
+                    "; SSL Mode=Require; Trust Server Certificate=true;";
+
+            }
+            services.AddDbContext<AshsDbContext>(options =>
+
+                options.UseNpgsql(connectionString)
+                    .UseSnakeCaseNamingConvention()
+                    .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                    .EnableSensitiveDataLogging()
+
+            );
+            services.AddControllers();
+            services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PostgresConnect", Version = "v1" });
@@ -46,7 +80,10 @@ namespace PostgresConnect
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PostgresConnect v1"));
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PostgresConnect v1");
+                    //c.RoutePrefix = "";
+                    });
             }
 
             app.UseRouting();
